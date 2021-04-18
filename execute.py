65750612bfbd494dfb7c33e2eeb1df603ecc0037
Satyam Dey <satyam.dey.23@gmail.com>
2021-04-18 23:50:00 +0530

    updated execute.py

--- execute.py
+++ execute.py
@@ -1,17 +1,36 @@
 import tensorflow
 from sklearn.externals import joblib
+import cv2
+import argparse
+from pathlib import Path
 
-from face_recognition_attendance.recognition import *
-from face_recognition_attendance.embedding import *
-from face_recognition_attendance.detect import *
+from recognition.recognition import *
+from recognition.embedding import *
+from recognition.detect import *
+
+from liveness.detection import *
+
+def spoof(img, model):
+  image = cv2.imread(img)
+  faceCascade = cv2.CascadeClassifier('/content/Face_Attendance_Colab/liveness/haarcascade_frontalface_default.xml')
+  return predictperson(image, model, faceCascade)
 
 def recog(img, model):
-  model_sk = joblib.load('/content/face_recognition_attendance/model/8th_sem_faces.pkl')
+  model_sk = joblib.load('/content/Face_Attendance_Colab/recognition/model/8th_sem_faces.pkl')
   image = extract_face(img)
   face_emb = get_embedding(model, image)
-  fetch_roll(img, model_sk, face_emb)
+  return fetch_roll(img, model_sk, face_emb)
 
 if __name__ == "__main__":
-  model = tensorflow.keras.models.load_model('/content/facenet_keras.h5')
-  img = "/content/frame72.jpg"
-  recog(img, model)
+  model_recog = tensorflow.keras.models.load_model('/content/Face_Attendance_Colab/recognition/model/facenet_keras.h5')
+  model_spoof = tensorflow.keras.models.load_model('/content/Face_Attendance_Colab/liveness/models/spoof.hdf5')
+  parser = argparse.ArgumentParser()
+  parser.add_argument("file_path")
+  p = parser.parse_args()
+  img = p.file_path
+  args = vars(parser.parse_args())
+  spoof_result = spoof(args["file_path"], model_spoof)
+  if spoof_result == 'real':
+    print(recog(img, model_recog))
+  else:
+    print(spoof_result)
